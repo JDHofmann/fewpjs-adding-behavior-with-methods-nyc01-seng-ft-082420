1a37e21948fd856fbf170b8d09f43c0c18d6157f
jdhofmann <justindhofmann@gmail.com>
2020-10-17 13:08:18 -0400

    done

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Your code here
+class Cat {
+  constructor(name){
+    this.name = name;
+  }
+  speak(){
+    console.log(`${this.name} says meow!`)
+  }
+}
+
+class Dog {
+  constructor(name){
+    this.name = name;
+  }
+  speak(){
+    console.log(`${this.name} says Woof!`)
+  }
+}
+
+class Bird {
+  constructor(name, sex){
+    this.name = name;
+    this.sex = sex;
+  }
+  speak(){
+    if(this.sex == "male"){
+      console.log(`It's me! ${this.name}, the parrot`)
+    } else {
+      console.log(`${this.name} says squak!`)
+    }
+  }
+}
